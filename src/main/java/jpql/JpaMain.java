48908333681e322afb5459ffdefc9b1ae9eb8fcc
SeJongDeveloper <sejongdeveloper@gmail.com>
2021-05-29 12:09:13 +0900

    1

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -25,30 +25,28 @@ public class JpaMain {
             Member member1 = new Member();
             member1.setUsername("회원1");
             member1.setTeam(teamA);
+            member1.setAge(0);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
             member2.setTeam(teamA);
+            member2.setAge(0);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
+            //FLUSH
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
             em.clear();
-
-            String query = "select m from Member m where m.team = :team";
-
-            List<Member> members = em.createQuery(query, Member.class)
-                    .setParameter("team", teamA)
-                    .getResultList();
-
-            for (Member member : members) {
-                System.out.println("member = " + member);
-            }
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
